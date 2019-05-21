b9423256c74faa1abcf9dcdabbb2fe90cc5a3d5c
invisible12138 <1637433370@qq.com>
2019-05-21 20:51:38 +0800

    挪格子

--- some_functions/main.py
+++ some_functions/main.py
@@ -1,6 +1,6 @@
 import numpy as np
-def gridmobile(spacematrix,xspeedmatrix,yspeedmatrix,length,parametermatrix,deltat):
-    (x,y)=np.shape(spacematrix)
+def gridmobile(xspeedmatrix,yspeedmatrix,length,parametermatrix,deltat):
+    (x,y)=np.shape(xspeedmatrix)
     for i in range(x):
         for j in range(y):
             parameterbefore=parametermatrix(i,j)
@@ -15,13 +15,13 @@ def gridmobile(spacematrix,xspeedmatrix,yspeedmatrix,length,parametermatrix,delt
             S3=(1-deltax%length)*deltay%length
             S4=deltax%length*deltay%length
             newparametermatrix= np.zeros(x,y)
-            newparametermatrix[x,y]+=parameterbefore*S1
-            newparametermatrix[x+1,y]+=parameterbefore*S2
-            newparametermatrix[x,y+1]+=parameterbefore*S3
-            newparametermatrix[x+1,y+1]+=parameterbefore*S4
+            newparametermatrix[i,j]+=parameterbefore*S1
+            newparametermatrix[i+1,j]+=parameterbefore*S2
+            newparametermatrix[i,j+1]+=parameterbefore*S3
+            newparametermatrix[i+1,j+1]+=parameterbefore*S4
     return (newparametermatrix)
-def avgr(spacematrix,xspeedmatrix,yspeedmatrix,length,rmatrix,Nmatrix,deltat):
-    (x,y)=np.shape(spacematrix)
+def avgr(xspeedmatrix,yspeedmatrix,length,rmatrix,Nmatrix,deltat):
+    (x,y)=np.shape(xspeedmatrix)
     for i in range(x):
         for j in range(y):
             rbefore=rmatrix[i,j]
@@ -38,14 +38,14 @@ def avgr(spacematrix,xspeedmatrix,yspeedmatrix,length,rmatrix,Nmatrix,deltat):
             S4=deltax%length*deltay%length
             newrmatrix= np.zeros(x,y)
             newNmatrix=np.zeros(x,y)
-            newrmatrix[x,y]+=rbefore**3*S1*Nbefore
-            newrmatrix[x+1,y]+=rbefore**3*S2*Nbefore
-            newrmatrix[x,y+1]+=rbefore**3*S3*Nbefore
-            newrmatrix[x+1,y+1]+=rbefore**3*S4*Nbefore
-            newNmatrix[x,y]+=Nbefore*S1
-            newNmatrix[x+1,y]+=Nbefore*S2
-            newNmatrix[x,y+1]+=Nbefore*S3
-            newNmatrix[x+1,y+1]+=Nbefore*S4
+            newrmatrix[i,j]+=rbefore**3*S1*Nbefore
+            newrmatrix[i+1,j]+=rbefore**3*S2*Nbefore
+            newrmatrix[i,j+1]+=rbefore**3*S3*Nbefore
+            newrmatrix[i+1,j+1]+=rbefore**3*S4*Nbefore
+            newNmatrix[i,j]+=Nbefore*S1
+            newNmatrix[i+1,j]+=Nbefore*S2
+            newNmatrix[i,j+1]+=Nbefore*S3
+            newNmatrix[i+1,j+1]+=Nbefore*S4
     for i in range(x):
         for j in range(y):
             newrmatrix[i][j]=(newrmatrix[i][j]/newNmatrix[i][j])**(1/3)
